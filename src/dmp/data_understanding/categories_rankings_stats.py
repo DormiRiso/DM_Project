eab3533cbadbf1d4f5e862f89d020c0a19bab038
dormi_riso <leonimichelangelo17@gmail.com>
2025-11-05 13:15:05 +0100

    Implement initial structure for category statistics analysis

---
+++ src/dmp/data_understanding/categories_rankings_stats.py
@@ -0,0 +1,14 @@
+from dmp.utils import save_figure
+
+def number_of_categories_dist(ranks_column):
+
+    lenght_list = []
+
+    for entry in ranks_column:
+        #strippo la lista in entrata dagli 0
+        #aggiungo la sua lunghezza a lenght_lsit
+    
+    #creo un hist per lenght_list
+
+def category_couples_heatmap():
+    pass
