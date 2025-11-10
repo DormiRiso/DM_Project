01fc3b18be7d4e677e4ade5ce41ed2b168fe7c74
dormi_riso <leonimichelangelo17@gmail.com>
2025-11-10 18:26:08 +0100

    implementata la funzione che calcola sse in funzione di k

--- src/dmp/data_clustering/__init__.py
+++ src/dmp/data_clustering/__init__.py
@@ -1,7 +1,8 @@
-from .k_mean import k_means_scatter
+from .k_mean import k_means_scatter, sse_vs_k
 from .cluster_df import cluster_df
 
 __all__ = [
     "k_means_scatter",
     "cluster_df",
+    "sse_vs_k",
 ]
--- src/dmp/data_clustering/cluster_df.py
+++ src/dmp/data_clustering/cluster_df.py
@@ -1,6 +1,7 @@
-from .k_mean import k_means_scatter
+from .k_mean import k_means_scatter, sse_vs_k
 from dmp.config import VERBOSE
 from dmp.utils import save_figure
+import matplotlib.pyplot as plt
 
 def cluster_df(df):
     """Funzione che esegue la clusterizzazione dei dati
@@ -11,7 +12,12 @@ def cluster_df(df):
     y_columns = ["ComWeight", "NumOwned", "LanguageEase"]
     k_list = [5, 5, 5]
     for x_column, y_column, k in zip(x_columns, y_columns, k_list):
-        plt = k_means_scatter(df[x_column], df[y_column], k, max_iters=5, x_label=x_column, y_label=y_column)
-        file_path = save_figure(plt, f'k_means_{x_column}_vs_{y_column}', folder="figures/clustered_scatters", extension=".png")
+        plt1 = k_means_scatter(df[x_column], df[y_column], k, max_iters=5, x_label=x_column, y_label=y_column)
+        file_path = save_figure(plt1, f'k_means_{x_column}_vs_{y_column}', folder="figures/clustered_scatters", extension=".png")
         if VERBOSE:
             print(f'Scatter plot del k-means salvato in: {file_path}')
+        # ATTENZIONE: la scelta di ending_k e max_iters potrebbe portare a runtime elevati
+        plt2, _ = sse_vs_k(df[x_column], df[y_column], ending_k=10, max_iters=10, x_label="k", y_label="SSE", title=f'SSE vs k per {x_column} vs {y_column}')
+        file_path = save_figure(plt2, f'sse_vs_k_{x_column}_vs_{y_column}', folder="figures/clustered_scatters", extension=".png")
+        if VERBOSE:
+            print(f'Plot SSE vs k salvato in: {file_path}')
